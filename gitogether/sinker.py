b1b874e0089b804b969e565ee4ec6698f3aa8dad
nklapste <nklapste@ualberta.ca>
2018-09-13 14:39:44 -0600

    updating dev code to start easier

--- gitogether/sinker.py
+++ gitogether/sinker.py
@@ -4,28 +4,51 @@
 """DEC CODE"""
 
 import os
-from git.repo import Repo
 import time
 
-BASE_DIR = "../../../gitdumbtest"
-while True:
-    bare_repo = Repo(os.path.join(BASE_DIR, 'test-repo'))
-    # bare_repo.create_remote("origin", "https://github.com/nklapste/gittestdumb.git")
-    origin = bare_repo.remote("origin")
-    origin.set_url("https://github.com/nklapste/gittestdumb.git")
+import git
 
-    index = bare_repo.index
-    index.add(["test.txt"])
-    time.sleep(0.5)
-    if bare_repo.index.diff(bare_repo.head.commit):
-        print("commiting")
-        index.commit("ignore")
-        origin.push()
+BASE_DIR = "../../../gitdumbtest2"
+GIT_REPO_URL = "https://github.com/nklapste/gittestdumb.git"
 
-    for fetch_info in origin.fetch():
 
-        if bare_repo.commit().hexsha == fetch_info.commit.hexsha:
-            print("up to date with origin")
-        else:
-            print("out of date with origin")
-            origin.pull()
+def setup_git():
+    os.makedirs(BASE_DIR, exist_ok=True)
+    test_repo = git.Repo.init(os.path.join(BASE_DIR, 'empty'))
+    try:
+        origin = test_repo.create_remote('origin', GIT_REPO_URL)
+    except:
+        origin = test_repo.remote("origin")
+
+    assert origin.exists()
+    assert origin == test_repo.remotes.origin == test_repo.remotes['origin']
+    origin.fetch()
+    test_repo.create_head('master', origin.refs.master)
+    test_repo.heads.master.set_tracking_branch(origin.refs.master)
+    test_repo.heads.master.checkout()
+    origin.set_url(GIT_REPO_URL)
+
+    return test_repo, origin
+
+
+def main():
+    test_repo, origin = setup_git()
+    while True:
+
+        time.sleep(0.5)
+        if test_repo.index.diff(test_repo.head.commit):
+            print("commiting")
+            test_repo.index.commit("ignore")
+            origin.push()
+
+        for fetch_info in origin.fetch():
+
+            if test_repo.commit().hexsha == fetch_info.commit.hexsha:
+                print("up to date with origin")
+            else:
+                print("out of date with origin")
+                origin.pull()
+
+
+if __name__ == "__main__":
+    main()
